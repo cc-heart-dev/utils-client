8b4740aa5ef01990835c00f41290edef0e23a95f
heart <7362469@qq.com>
2023-03-18 22:06:18 +0800

    chore: add dts output

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@ import tsConfig from "./tsconfig.json" assert { type: "json" };
 import typescript from "@rollup/plugin-typescript";
 
 tsConfig.compilerOptions.declarationDir = './types';
-tsConfig.compilerOptions.declaration = false
-console.log(tsConfig);
+tsConfig.compilerOptions.declaration = true
+tsConfig.compilerOptions.emitDeclarationOnly = true
+
 export default {
   input: "index.ts",
   output: [
