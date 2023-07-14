8e32e7ae92932057d06bde41f7bcfbc215ba8cc2
heart <7362469@qq.com>
2023-07-14 23:37:02 +0800

    feat: add cjs modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import tsConfig from "./tsconfig.json" assert { type: "json" };
 import typescript from "@rollup/plugin-typescript";
 
-tsConfig.compilerOptions.declarationDir = './types';
-tsConfig.compilerOptions.declaration = true
-tsConfig.compilerOptions.emitDeclarationOnly = true
+tsConfig.compilerOptions.declaration = false;
 
 export default {
   input: "index.ts",
   output: [
     {
-      file: "dist/index.js",
+      file: "dist/esm/index.js",
       format: "esm",
+    },
+    {
+      file: "dist/cjs/index.cjs",
+      format: "cjs",
     },
   ],
   plugins: [typescript(tsConfig)],
